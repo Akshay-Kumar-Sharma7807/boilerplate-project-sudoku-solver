17473c10e2cf42960b2f4f89fad39d86a7e490c9
Akshay Kumar Sharma <sharmaakshaykumar7807@gmail.com>
2021-06-26 19:44:39 +0530

    making logic

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,41 @@
 class SudokuSolver {
-
   validate(puzzleString) {
+    if (puzzleString.length === 81) {
+      var puzzleReg = /[^0-9.]/g;
+      var puzzle = puzzleReg.test(puzzleString);
+      if (!puzzle) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return "error length";
+    }
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    var testArray = puzzleString.split("");
+    var puzzleArray = [];
+    var blockArray = [];
+    var rowArray = [];
+    testArray.map((value, index) => {
+      if (index % 3 === 0 && index !== 0) {
+        blockArray.push(rowArray);
+        rowArray = [];
+      }
+      rowArray.push(value);
+    });
+    blockArray.push(rowArray);
+    console.log("testArray", testArray);
+    console.log("rowarray", rowArray);
+    console.log("blockArray", blockArray);
   }
 
-  checkColPlacement(puzzleString, row, column, value) {
+  checkColPlacement(puzzleString, row, column, value) {}
 
-  }
+  checkRegionPlacement(puzzleString, row, column, value) {}
 
-  checkRegionPlacement(puzzleString, row, column, value) {
-
-  }
-
-  solve(puzzleString) {
-    
-  }
+  solve(puzzleString) {}
 }
 
 module.exports = SudokuSolver;
-
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,34 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require("../controllers/sudoku-solver.js");
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {
+    console.log(req.body);
+    var { puzzle, coordinate, value } = req.body;
+    if (
+      puzzle === undefined ||
+      coordinate === undefined ||
+      value === undefined
+    ) {
+      res.json({ error: "Required field(s) missing" });
+    }
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+    solver.checkRowPlacement(puzzle, coordinate[0], coordinate[1], value);
 
-    });
+    var validate = solver.validate(puzzle);
+    if (validate) {
+      res.json({ valid: true });
+    } else if (validate === "error length") {
+      res.json({ error: "Expected puzzle to be 81 characters long" });
+    } else {
+      res.json({ error: "Invalid characters in puzzle" });
+    }
+  });
+
+  app.route("/api/solve").post((req, res) => {
+    console.log(req.body);
+  });
 };

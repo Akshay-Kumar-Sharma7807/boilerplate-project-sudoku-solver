14268f008bf102b22e4a62798499a34d37900308
Akshay Kumar Sharma <sharmaakshaykumar7807@gmail.com>
2021-06-29 15:47:11 +0530

    first algorithm

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -13,36 +13,101 @@ class SudokuSolver {
     }
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
-    var testArray = puzzleString.split("");
-    var puzzleArray = [];
-    var blockArray = [];
-    var rowArray = [];
-    testArray.map((value, index) => {
-      if (index % 3 === 0 && index !== 0) {
-        blockArray.push(rowArray);
-        rowArray = [];
-      }
-      rowArray.push(value);
-    });
-    blockArray.push(rowArray);
-    console.log("testArray", testArray);
-    console.log("rowarray", rowArray);
-    console.log("blockArray", blockArray);
-    blockArray.map((value, index) => {
-      if (index % 3 === 0 && index !== 0) {
-        blockArray.push(rowArray);
-        rowArray = [];
-      }
-      rowArray.push(value);
-    });
+  checkRowPlacement(puzzleString, row, column, value, coordinate) {
+    // var puzzleArray = puzzleString.split("");
+    // var blockArray = [];
+    // var rowArray = [];
+    // puzzleArray.map((value, index) => {
+    //   if (index % 3 === 0 && index !== 0) {
+    //     blockArray.push(rowArray);
+    //     rowArray = [];
+    //   }
+    //   rowArray.push(value);
+    // });
+    // blockArray.push(rowArray);
+
+    // console.log("puzzleArray", puzzleArray);
+    // console.log("blockArray", blockArray);
+    // blockArray.map((value, index) => {
+    //   if (index % 3 === 0 && index !== 0) {
+    //     blockArray.push(rowArray);
+    //     rowArray = [];
+    //   }
+    //   rowArray.push(value);
+    // });
+
+    // let columncoordinate = coordinate.charCodeAt(0) - 64;
+    // let rowcoordinate = coordinate[1];
+    // let regioncoordinate = Math.ceil(rowcoordinate / 3);
+    // console.log(regioncoordinate);
+    // let colregioncoordinate = Math.ceil(columncoordinate / 3);
+    // console.log(colregioncoordinate);
+    // for(var i = regioncoordinate===1?1:regioncoordinate+2)
+    // ----------------------------------------
+    var rowIndex = row.indexOf(value);
+    if (rowIndex === -1) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
-  checkColPlacement(puzzleString, row, column, value) {}
+  checkColPlacement(puzzleString, row, column, value) {
+    var columnIndex = column.indexOf(value);
+    if (columnIndex === -1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-  checkRegionPlacement(puzzleString, row, column, value) {}
+  checkRegionPlacement(rowArray, row, column, value) {
+    row = Math.floor(row / 3) * 3;
+    column = Math.floor(column / 3) * 3;
+    var isNotFound = true;
+    // console.log("rowarray", rowArray);
+    // console.log("row", row);
+    // console.log("column", column);
 
-  solve(puzzleString) {}
+    for (var i = 0; i < 3; i++) {
+      for (var j = 0; j < 3; j++) {
+        // console.log("value", rowArray[row + i][column + j]);
+        if (rowArray[row + i][column + j] === value) {
+          isNotFound = false;
+        }
+        // console.log(row);
+        // console.log(rowArray[row + i]);
+      }
+    }
+    return isNotFound;
+  }
+
+  solve(rowArray) {
+    console.log(rowArray);
+    rowArray.map((row, rowIndex) => {
+      row.map((value, index) => {
+        if (value === ".") {
+          var columnArray = [];
+          rowArray.map((valuer) => {
+            columnArray.push(valuer[index]);
+          });
+          // console.log("columnarray", columnArray);
+          for (var i = 1; i < 10; i++) {
+            if (
+              this.checkColPlacement("ks", "row", columnArray, i + "") &&
+              this.checkRowPlacement("kfsf", row, columnArray, i + "") &&
+              this.checkRegionPlacement(rowArray, rowIndex, index, i + "")
+            ) {
+              // console.log(i);
+              row[index] = i + "";
+              break;
+            }
+          }
+        }
+      });
+    });
+    return rowArray;
+  }
 }
 
 module.exports = SudokuSolver;
--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,6 @@ module.exports = function (app) {
   let solver = new SudokuSolver();
 
   app.route("/api/check").post((req, res) => {
-    console.log(req.body);
     var { puzzle, coordinate, value } = req.body;
     if (
       puzzle === undefined ||
@@ -15,12 +14,56 @@ module.exports = function (app) {
     ) {
       res.json({ error: "Required field(s) missing" });
     }
+    var puzzleArray = puzzle.split("");
+    var rowArray = [];
+    var tempArray = [];
+    puzzleArray.map((value, index) => {
+      if (index % 9 === 0 && index !== 0) {
+        rowArray.push(tempArray);
+        tempArray = [];
+      }
+      tempArray.push(value);
+    });
+    rowArray.push(tempArray);
 
-    solver.checkRowPlacement(puzzle, coordinate[0], coordinate[1], value);
+    var columnArray = [];
+    rowArray.map((value) => {
+      columnArray.push(value[coordinate[1] - 1]);
+    });
+    // solver.checkRowPlacement(
+    //   puzzle,
+    //   rowArray[coordinate.charCodeAt(0) - 65],
+    //   columnArray,
+    //   value
+    // );
 
     var validate = solver.validate(puzzle);
     if (validate) {
-      res.json({ valid: true });
+      if (
+        solver.checkRowPlacement(
+          puzzle,
+          rowArray[coordinate.charCodeAt(0) - 65],
+          columnArray,
+          value,
+          coordinate
+        ) &&
+        solver.checkColPlacement(
+          puzzle,
+          rowArray[coordinate.charCodeAt(0) - 65],
+          columnArray,
+          value
+        ) &&
+        solver.checkRegionPlacement(
+          rowArray,
+          coordinate.charCodeAt(0) - 65,
+          coordinate[1],
+          value
+        )
+      ) {
+        res.json({ valid: true });
+      } else {
+        res.json({ valid: false });
+      }
     } else if (validate === "error length") {
       res.json({ error: "Expected puzzle to be 81 characters long" });
     } else {
@@ -29,6 +72,27 @@ module.exports = function (app) {
   });
 
   app.route("/api/solve").post((req, res) => {
-    console.log(req.body);
+    var puzzle = req.body.puzzle;
+    var puzzleArray = puzzle.split("");
+    var rowArray = [];
+    var tempArray = [];
+    puzzleArray.map((value, index) => {
+      if (index % 9 === 0 && index !== 0) {
+        rowArray.push(tempArray);
+        tempArray = [];
+      }
+      tempArray.push(value);
+    });
+    rowArray.push(tempArray);
+    var sol = solver.solve(rowArray);
+    var final = [];
+    sol.map((value, index) => {
+      value.map((value) => {
+        final.push(value);
+      });
+    });
+    // console.log(sol);
+    // console.log(final);
+    res.json({ solution: final });
   });
 };
